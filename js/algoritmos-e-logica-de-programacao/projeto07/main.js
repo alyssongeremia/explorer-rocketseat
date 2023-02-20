7bdf4d5f382f0085307176b6c12d71679b804019
Alysson Geremia <104856858+alyssongeremia@users.noreply.github.com>
2023-02-20 10:46:01 -0300

    complete challenge spa

--- js/algoritmos-e-logica-de-programacao/projeto07/main.js
+++ js/algoritmos-e-logica-de-programacao/projeto07/main.js
@@ -1,7 +1,8 @@
 const routes = {
-    "/": "/pages/home.html",
-    "/universe": "/pages/universe.html",
-    "/exploration": "/pages/exploration.html"
+    "/": "./js/algoritmos-e-logica-de-programacao/projeto07/pages/home.html",
+    "/universe": "/js/algoritmos-e-logica-de-programacao/projeto07/pages/universe.html",
+    "/exploration": "js/algoritmos-e-logica-de-programacao/projeto07/pages/exploration.html",
+    404: "./pages/404.html",
 }
 
 function route(event) {
@@ -14,13 +15,34 @@ function route(event) {
 }
 
 function handle() {
-    const { pathname } = window.location // desestruturado
+    const { pathname } = window.location
     const route = routes[pathname] || routes[404]
-
     fetch(route).then(data => data.text()).then(html => {
-        document.querySelector('#app').innerHTML = html
-    })
-
+            document.querySelector('#app').innerHTML = html
+        })
 }
 
 handle()
+
+const btnHome = document.querySelector('.btn-home')
+const btnUniverse = document.querySelector('.btn-universe')
+const btnExploration = document.querySelector('.btn-exploration')
+const bgPage = document.querySelector('.page')
+
+btnHome.addEventListener('click', function() {
+    bgPage.classList.add('bg-home')
+    bgPage.classList.remove('bg-universe')
+    bgPage.classList.remove('bg-exploration')
+})
+
+btnUniverse.addEventListener('click', function() {
+    bgPage.classList.add('bg-universe')
+    bgPage.classList.remove('bg-home')
+    bgPage.classList.remove('bg-exploration')
+})
+
+btnExploration.addEventListener('click', function() {
+    bgPage.classList.add('bg-exploration')
+    bgPage.classList.remove('bg-home')
+    bgPage.classList.remove('bg-universe')
+})
